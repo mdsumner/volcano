9a6611d20a07a5685db75d9fca16fd520ee1f95e
Michael Sumner <mdsumner@gmail.com>
2022-05-11 19:54:48 +1000

    volcano

---
+++ R/volcano-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
+#' The georeferenced volcano data.
+#'
+#' In [image()] xyz list format.
+#' @docType data
+#' @name volc
+#' @examples
+#' image(volc, asp = 1)
+NULL
